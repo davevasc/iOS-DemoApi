9781922848216b08eb901ab1602ca7973e77b3c6
dave <daveplus@gmail.com>
2023-10-23 22:22:57 +0200

    Initial Commit

---
+++ DemoApi/DemoApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApiApp.swift
+//  DemoApi
+//
+//  Created by David Vicente on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
